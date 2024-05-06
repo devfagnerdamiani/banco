5e7f1b85e9e556f049b48ec95126dde8e6418515
Fagner Damiani <fagnerdamiani@gmail.com>
2024-05-06 16:15:38 -0400

    AULA 9 - Pacotes e Visibilidade

--- main.go
+++ main.go
@@ -1,61 +1,17 @@
-// AULA 8 - Transferencia entre contas | GO - Orientado a Objetos
+// AULA 9 - Pacotes e Visibilidade | GO - Orientado a Objetos
 
 package main
 
-import "fmt"
-
-type ContaCorrente struct {
-	titular       string
-	numeroAgencia int
-	numeroDaConta int
-	saldo         float64
-}
-
-func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
-
-	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
-
-	if podeSacar {
-
-		c.saldo -= valorDoSaque
-		return "Saque realizado com sucesso!"
-	} else {
-		return "Saldo insuficiente!"
-	}
-}
-
-func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
-
-	if valorDoDeposito > 0 {
-
-		c.saldo += valorDoDeposito
-
-		return "Deposito realizado com sucesso! Valor atual do saldo: ", c.saldo
-
-	} else {
-
-		return "Deposito menor que zero. Valor atual do saldo: ", c.saldo
-
-	}
-
-}
-
-func (c *ContaCorrente) Transferir(valorDaTransferencia float64, contaDestino *ContaCorrente) bool {
-	if valorDaTransferencia < c.saldo && valorDaTransferencia > 0 {
-		c.saldo -= valorDaTransferencia
-		contaDestino.Depositar(valorDaTransferencia)
-		return true
-	} else {
-
-		return false
-
-	}
-
-}
+import (
+	"banco/contas" // Pacote personalizado
+	// c	"banco/contas" // Pacote personalizado com apelido "c"
+	"fmt"
+)
 
 func main() {
-	contaDaSilvia := ContaCorrente{titular: "Silvia", saldo: 300}
-	contaDoGustavo := ContaCorrente{titular: "Gustavo", saldo: 100}
+
+	contaDaSilvia := contas.ContaCorrente{Titular: "Silvia", Saldo: 300}
+	contaDoGustavo := contas.ContaCorrente{Titular: "Gustavo", Saldo: 100}
 
 	fmt.Println(contaDaSilvia, contaDoGustavo)
 
