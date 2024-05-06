470d32de7d0d7ef3d2663e783bfdcab0f9202261
Fagner Damiani <fagnerdamiani@gmail.com>
2024-05-06 14:04:42 -0400

    AULA 5 - COMPARANDO TIPOS

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// AULA 4 - New e Ponteiros | GO - Orientado a Objetos
+// AULA 5 - Comparando Tipos | GO - Orientado a Objetos
 
 package main
 
@@ -12,17 +12,38 @@ type ContaCorrente struct {
 }
 
 func main() {
-	//	contaDoGuilherme := ContaCorrente{titular: "Guilherme", numeroAgencia: 589, numeroDaConta: 123456, saldo: 125.5}
-	contaDoGuilherme := ContaCorrente{titular: "Guilherme", saldo: 125.5}
-	contaDaBruna := ContaCorrente{"Bruna", 222, 111222, 200.0}
+	// contaDoGuilherme := ContaCorrente{titular: "Guilherme", numeroAgencia: 589, numeroDaConta: 123456, saldo: 125.5}
+	// contaDoGuilherme2 := ContaCorrente{titular: "Guilherme", numeroAgencia: 580, numeroDaConta: 123456, saldo: 125.5}
+	//	contaDoGuilherme := ContaCorrente{titular: "Guilherme", saldo: 125.5}
+	//contaDaBruna := ContaCorrente{"Bruna", 222, 111222, 200.0}
+	//contaDaBruna2 := ContaCorrente{"Bruna", 222, 111111, 200.0}
 
-	fmt.Println(contaDoGuilherme)
-	fmt.Println(contaDaBruna)
+	// fmt.Println(contaDoGuilherme)
+	// fmt.Println(contaDoGuilherme2)
+	//fmt.Println(contaDaBruna)
 
 	var contaDaCris *ContaCorrente
 	contaDaCris = new(ContaCorrente)
 	contaDaCris.titular = "Cris"
+	contaDaCris.saldo = 500
+
+	var contaDaCris2 *ContaCorrente
+	contaDaCris2 = new(ContaCorrente)
+	contaDaCris2.titular = "Cris"
+	contaDaCris2.saldo = 500
+
+	fmt.Println(contaDaCris)
+	fmt.Println(contaDaCris)
+
+	fmt.Println(&contaDaCris)
+	fmt.Println(&contaDaCris)
 
 	fmt.Println(*contaDaCris)
+
+	//fmt.Println(contaDoGuilherme == contaDoGuilherme2)
+	//fmt.Println(contaDaBruna == contaDaBruna2)
+
+	fmt.Println(contaDaCris == contaDaCris2)
+	fmt.Println(*contaDaCris == *contaDaCris2)
 
 }
