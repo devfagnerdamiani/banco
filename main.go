9d04dd148b7631f32cb1e1adc4637bb60b2b6c93
Fagner Damiani <fagnerdamiani@gmail.com>
2024-05-06 13:17:04 -0400

    AULA 2 - STRUCTS

--- main.go
+++ main.go
@@ -1,23 +1,18 @@
-// AULA 1 - Variáveis e Tipos | GO - Orientado a Objetos
+// AULA 2 - STRUCTS | GO - Orientado a Objetos
 
 package main
 
 import "fmt"
 
+type ContaCorrente struct {
+	titular       string
+	numeroAgencia int
+	numeroDaConta int
+	Saldo         float64
+}
+
 func main() {
 
-	var titular string = "Guilherme"
-	var numeroAgencia int = 589
-	var numeroDaConta int = 123456
-	var saldo float64 = 125.5
-
-	fmt.Println("Titular: ", titular, " | Número da Agência: ", numeroAgencia, " | Número da Conta: ", numeroDaConta, " | Saldo: ", saldo)
-
-	var titular2 string = "Luciene"
-	var numeroAgencia2 int = 555
-	var numeroDaConta2 int = 111333
-	var saldo2 float64 = 200.0
-
-	fmt.Println("Titular: ", titular2, " | Número da Agência: ", numeroAgencia2, " | Número da Conta: ", numeroDaConta2, " | Saldo: ", saldo2)
+	fmt.Println(ContaCorrente{})
 
 }
