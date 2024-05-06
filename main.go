02b7e6f2ac53d4e6d12fc3635633fe5722cbdc4f
Fagner Damiani <fagnerdamiani@gmail.com>
2024-05-06 15:17:28 -0400

    AULA 6 - CRIANDO O METODO SACAR

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// AULA 5 - Comparando Tipos | GO - Orientado a Objetos
+// AULA 6 - Criando o método sacar | GO - Orientado a Objetos
 
 package main
 
@@ -11,39 +11,29 @@ type ContaCorrente struct {
 	saldo         float64
 }
 
+func (c *ContaCorrente) sacar(valorDoSaque float64) string {
+
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+
+	if podeSacar {
+
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso!"
+	} else {
+		return "Saldo insuficiente!"
+	}
+
+}
+
 func main() {
-	// contaDoGuilherme := ContaCorrente{titular: "Guilherme", numeroAgencia: 589, numeroDaConta: 123456, saldo: 125.5}
-	// contaDoGuilherme2 := ContaCorrente{titular: "Guilherme", numeroAgencia: 580, numeroDaConta: 123456, saldo: 125.5}
-	//	contaDoGuilherme := ContaCorrente{titular: "Guilherme", saldo: 125.5}
-	//contaDaBruna := ContaCorrente{"Bruna", 222, 111222, 200.0}
-	//contaDaBruna2 := ContaCorrente{"Bruna", 222, 111111, 200.0}
+	contaDaSilvia := ContaCorrente{}
+	contaDaSilvia.titular = "Silvia"
+	contaDaSilvia.saldo = 500
 
-	// fmt.Println(contaDoGuilherme)
-	// fmt.Println(contaDoGuilherme2)
-	//fmt.Println(contaDaBruna)
-
-	var contaDaCris *ContaCorrente
-	contaDaCris = new(ContaCorrente)
-	contaDaCris.titular = "Cris"
-	contaDaCris.saldo = 500
-
-	var contaDaCris2 *ContaCorrente
-	contaDaCris2 = new(ContaCorrente)
-	contaDaCris2.titular = "Cris"
-	contaDaCris2.saldo = 500
-
-	fmt.Println(contaDaCris)
-	fmt.Println(contaDaCris)
-
-	fmt.Println(&contaDaCris)
-	fmt.Println(&contaDaCris)
-
-	fmt.Println(*contaDaCris)
-
-	//fmt.Println(contaDoGuilherme == contaDoGuilherme2)
-	//fmt.Println(contaDaBruna == contaDaBruna2)
-
-	fmt.Println(contaDaCris == contaDaCris2)
-	fmt.Println(*contaDaCris == *contaDaCris2)
-
+	fmt.Println(contaDaSilvia.saldo)
+	fmt.Println(contaDaSilvia.sacar(800.0))
+	fmt.Println(contaDaSilvia.sacar(-100.0))
+	fmt.Println(contaDaSilvia.sacar(200.0))
+	fmt.Println(contaDaSilvia.sacar(400.0))
+	fmt.Println(contaDaSilvia.saldo)
 }
