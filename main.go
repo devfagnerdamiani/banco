463e8d1867e5f36ce92ad741b8a050bde7355822
Fagner Damiani <fagnerdamiani@gmail.com>
2024-05-07 09:04:00 -0400

    AULA 14 - INTERFACE

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// AULA 13 - Conta Poupanca | GO - Orientado a Objetos
+// AULA 14 - Interface | GO - Orientado a Objetos
 
 // go mod init // ########## CÓDIGO PARA CRIAR MÓDULOS NA PASTA RAÍZ ( Onde esta o arquivo main.go )
 
@@ -9,12 +9,29 @@ import (
 	"fmt"
 )
 
+func pagarBoleto(conta verificarConta, valorDoBoleto float64) {
+
+	conta.Sacar(valorDoBoleto)
+
+}
+
+type verificarConta interface {
+	Sacar(valor float64) string
+}
+
 func main() {
 
 	contaDoDenis := contas.ContaPoupanca{}
-	contaDaPati := contas.ContaPoupanca{}
+	contaDaLuisa := contas.ContaCorrente{}
 
-	fmt.Println(contaDoDenis)
-	fmt.Println(contaDaPati)
+	contaDoDenis.Depositar(100)
+	pagarBoleto(&contaDoDenis, 60)
+	fmt.Println(contaDoDenis.ObterSaldo())
+	//	fmt.Println(contaDoDenis)
+	// contaDoDenis.Sacar(5000) // Valor de teste a não ser sacado por conta do valor a sacar é superior ao valor do saldo
+	// contaDoDenis.Sacar(55)
+	contaDaLuisa.Depositar(500)
+	pagarBoleto(&contaDaLuisa, 400)
+	fmt.Println(contaDaLuisa.ObterSaldo())
 
 }
